20174c8c5349949f694b2fba7d9324da71fdb29b
haruki7049 <tontonkirikiri@gmail.com>
2023-10-01 07:53:25 +0900

    using the command, cargo fmt

--- dtm-database-backend/src/main.rs
+++ dtm-database-backend/src/main.rs
@@ -1,9 +1,5 @@
 use axum::{routing::get, Router};
-use std::net::{
-  IpAddr,
-  Ipv4Addr,
-  SocketAddr,
-};
+use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 const DEFAULT_PORT: u16 = 3001;
 const DEFAULT_IPV4: Ipv4Addr = Ipv4Addr::new(127, 0, 0, 1);
@@ -31,7 +27,9 @@ async fn main() {
 /// this function is not pure, and write out message to stdout.
 fn set_port(port_env: &str) -> u16 {
   match std::env::var(port_env) {
-    Ok(value) => value.parse::<u16>().expect("PORT environment variable should be u16, between 0 and 65535"),
+    Ok(value) => value
+      .parse::<u16>()
+      .expect("PORT environment variable should be u16, between 0 and 65535"),
     Err(error) => {
       println!("WARNING: {}. Using default port, {}", error, DEFAULT_PORT);
       DEFAULT_PORT
@@ -43,7 +41,9 @@ fn set_port(port_env: &str) -> u16 {
 /// this function is not pure, and write out message to stdout.
 fn set_ipv4(ipv4_env: &str) -> Ipv4Addr {
   match std::env::var(ipv4_env) {
-    Ok(value) => value.parse::<Ipv4Addr>().expect("IPV4 environment variable should be as '127.0.0.1', std::net::Ipv4Addr"), // TODO: IPv4 number is not pure number, but number array.
+    Ok(value) => value
+      .parse::<Ipv4Addr>()
+      .expect("IPV4 environment variable should be as '127.0.0.1', std::net::Ipv4Addr"), // TODO: IPv4 number is not pure number, but number array.
     Err(error) => {
       println!("WARNING: {}. Using default IP, {}", error, DEFAULT_IPV4);
       DEFAULT_IPV4
@@ -53,14 +53,7 @@ fn set_ipv4(ipv4_env: &str) -> Ipv4Addr {
 
 #[cfg(test)]
 mod test {
-  use crate::{
-    set_port,
-    set_ipv4,
-    DEFAULT_PORT,
-    DEFAULT_IPV4,
-    PORT_ENV,
-    IPV4_ENV,
-  };
+  use crate::{set_ipv4, set_port, DEFAULT_IPV4, DEFAULT_PORT, IPV4_ENV, PORT_ENV};
   use std::net::Ipv4Addr;
 
   /// set_port function's test. this test check whether set_port function's return value is equivalent DEFAULT_PORT or not.
