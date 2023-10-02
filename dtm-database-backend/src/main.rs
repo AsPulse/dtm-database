a82179772f5fdcca851211d9eff57e0952d9258a
haruki7049 <tontonkirikiri@gmail.com>
2023-10-02 12:23:03 +0900

    renamed set_port get_port

--- dtm-database-backend/src/main.rs
+++ dtm-database-backend/src/main.rs
@@ -7,7 +7,7 @@ const PORT_ENV: &str = "PORT";
 #[tokio::main]
 async fn main() {
   // set port, ipv4, and socket.
-  let port: u16 = set_port(PORT_ENV);
+  let port: u16 = get_port(PORT_ENV);
   let ipv4: Ipv4Addr = Ipv4Addr::UNSPECIFIED;
   let socket_v4: SocketAddr = SocketAddr::new(IpAddr::V4(ipv4), port);
 
@@ -21,9 +21,9 @@ async fn main() {
     .unwrap();
 }
 
-/// A port setter for this server which is hyper.
+/// A port getter for this server which is hyper.
 /// this function is not pure, and write out message to stdout.
-fn set_port(port_env: &str) -> u16 {
+fn get_port(port_env: &str) -> u16 {
   match std::env::var(port_env) {
     Ok(value) => value
       .parse::<u16>()
@@ -37,12 +37,12 @@ fn set_port(port_env: &str) -> u16 {
 
 #[cfg(test)]
 mod test {
-  use crate::{set_port, DEFAULT_PORT, PORT_ENV};
+  use crate::{get_port, DEFAULT_PORT, PORT_ENV};
 
-  /// set_port function's test. this test check whether set_port function's return value is equivalent DEFAULT_PORT or not.
+  /// get_port function's test. this test check whether get_port function's return value is equivalent DEFAULT_PORT or not.
   #[test]
-  fn test_set_default_port() {
-    let port: u16 = set_port(PORT_ENV);
+  fn test_get_default_port() {
+    let port: u16 = get_port(PORT_ENV);
     assert_eq!(port, DEFAULT_PORT);
   }
 }
