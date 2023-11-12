ce12977656def3b1843e6bc3645d7369b3181510
AsPulse <aspulse.x@gmail.com>
2023-11-12 13:15:03 +0900

    Using ENV, PORT const

--- dtm-database-backend/src/bin/main.rs
+++ dtm-database-backend/src/bin/main.rs
@@ -1,7 +1,7 @@
 extern crate dtm_database_backend;
-use dtm_database_backend::env::{get_booting_mode, get_port, BootingModes, MODE_ENV, PORT_ENV};
 
 use axum::{routing::get, Router};
+use dtm_database_backend::env::{PORT, ENV, BootingMode};
 use std::net::{Ipv4Addr, SocketAddr};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
@@ -11,15 +11,13 @@ const VERSION: &str = "0.0.1";
 #[tokio::main]
 async fn main() {
   // set port, ipv4, and socket.
-  let socket_v4: SocketAddr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, get_port(PORT_ENV)));
-  // set booting mode.
-  let booting_mode: BootingModes = get_booting_mode(MODE_ENV);
+  let socket_v4: SocketAddr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, *PORT));
 
   // build our application with a single route
-  let app = match booting_mode {
-    BootingModes::Debug => Router::new()
+  let app = match *ENV {
+    BootingMode::Debug => Router::new()
       .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi())),
-    BootingModes::Production => Router::new(),
+    BootingMode::Production => Router::new(),
   }
   .route("/", get(hello))
   .route("/version", get(version));
--- dtm-database-backend/src/env.rs
+++ dtm-database-backend/src/env.rs
@@ -1,73 +1,53 @@
-pub const PORT_ENV: &str = "PORT";
-pub const MODE_ENV: &str = "ENV";
+use once_cell::sync::Lazy;
+
+const PORT_ENV: &str = "PORT";
+const MODE_ENV: &str = "ENV";
 const DEBUG: &str = "DEBUG";
 const PRODUCTION: &str = "PRODUCTION";
 pub const DEFAULT_PORT: u16 = 3001;
-pub const DEFAULT_MODE: BootingModes = BootingModes::Debug;
+pub const DEFAULT_MODE: BootingMode = BootingMode::Debug;
 
-#[derive(Debug, PartialEq)]
-pub enum BootingModes {
-  Debug,
-  Production,
-}
-
-impl std::fmt::Display for BootingModes {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    match self {
-      BootingModes::Debug => write!(f, "{}", DEBUG),
-      BootingModes::Production => write!(f, "{}", PRODUCTION),
-    }
-  }
-}
-
-impl std::str::FromStr for BootingModes {
-  type Err = &'static str;
-
-  fn from_str(s: &str) -> Result<Self, Self::Err> {
-    match s {
-      DEBUG => Ok(BootingModes::Debug),
-      PRODUCTION => Ok(BootingModes::Production),
-      _ => Err("Unknown mode found"),
-    }
-  }
-}
-
-/// A booting mode getter for this server which is hyper.
-/// this function is not pure, and write out message to stdout.
-pub fn get_booting_mode(mode_env: &str) -> BootingModes {
-  match std::env::var(mode_env) {
-    Ok(value) => value.parse::<BootingModes>().unwrap(),
+pub const ENV: Lazy<BootingMode> = Lazy::new(|| {
+  match std::env::var(MODE_ENV) {
+    Ok(value) => value.parse::<BootingMode>().unwrap(),
     Err(_) => DEFAULT_MODE,
   }
-}
+});
 
-/// A port getter for this server which is hyper.
-/// this function is not pure, and write out message to stdout.
-pub fn get_port(port_env: &str) -> u16 {
-  match std::env::var(port_env) {
+pub const PORT: Lazy<u16> = Lazy::new(|| {
+  match std::env::var(PORT_ENV) {
     Ok(value) => value
       .parse::<u16>()
       .expect("PORT environment variable should be u16, between 0 and 65535"),
     Err(_) => DEFAULT_PORT,
   }
+});
+
+
+#[derive(Debug, PartialEq)]
+pub enum BootingMode {
+  Debug,
+  Production,
 }
 
-#[cfg(test)]
-mod test {
-  use crate::env::{
-    get_booting_mode, get_port, BootingModes, DEFAULT_MODE, DEFAULT_PORT, MODE_ENV, PORT_ENV,
-  };
-
-  /// get_port function's test. this test check whether get_port function's return value is equivalent DEFAULT_PORT or not.
-  #[test]
-  fn test_get_default_port() {
-    let port: u16 = get_port(PORT_ENV);
-    assert_eq!(port, DEFAULT_PORT);
-  }
-
-  #[test]
-  fn test_get_default_mode() {
-    let booting_mode: BootingModes = get_booting_mode(MODE_ENV);
-    assert_eq!(booting_mode, DEFAULT_MODE);
+impl std::fmt::Display for BootingMode {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    match self {
+      BootingMode::Debug => write!(f, "{}", DEBUG),
+      BootingMode::Production => write!(f, "{}", PRODUCTION),
+    }
   }
 }
+
+impl std::str::FromStr for BootingMode {
+  type Err = &'static str;
+
+  fn from_str(s: &str) -> Result<Self, Self::Err> {
+    match s {
+      DEBUG => Ok(BootingMode::Debug),
+      PRODUCTION => Ok(BootingMode::Production),
+      _ => Err("Unknown mode found"),
+    }
+  }
+}
+
