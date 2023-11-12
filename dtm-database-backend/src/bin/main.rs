0cd7d5840587076f8663030517493d55956c7f66
あすぱる <84216737+AsPulse@users.noreply.github.com>
2023-11-12 13:25:03 +0900

    [proposal] separated main.rs to with once_cell (#42)

--- dtm-database-backend/src/bin/main.rs
+++ dtm-database-backend/src/bin/main.rs
@@ -1,7 +1,7 @@
 extern crate dtm_database_backend;
-use dtm_database_backend::env::{get_booting_mode, get_port, BootingModes, MODE_ENV, PORT_ENV};
 
 use axum::{routing::get, Router};
+use dtm_database_backend::env::{BootingMode, ENV, PORT};
 use std::net::{Ipv4Addr, SocketAddr};
 use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
@@ -11,24 +11,23 @@ const VERSION: &str = "0.0.1";
 #[tokio::main]
 async fn main() {
   // set port, ipv4, and socket.
-  let socket_v4: SocketAddr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, get_port(PORT_ENV)));
-  // set booting mode.
-  let booting_mode: BootingModes = get_booting_mode(MODE_ENV);
+  let socket_v4: SocketAddr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, *PORT));
 
   // build our application with a single route
-  let app = match booting_mode {
-    BootingModes::Debug => Router::new()
+  let app = match *ENV {
+    BootingMode::Debug => Router::new()
       .merge(SwaggerUi::new("/swagger-ui").url("/api-docs/openapi.json", ApiDoc::openapi())),
-    BootingModes::Production => Router::new(),
+    BootingMode::Production => Router::new(),
   }
   .route("/", get(hello))
   .route("/version", get(version));
 
   // run it with hyper on localhost:3000
-  axum::Server::bind(&socket_v4)
-    .serve(app.into_make_service())
-    .await
-    .unwrap();
+  let server = axum::Server::bind(&socket_v4).serve(app.into_make_service());
+
+  println!("Listening on {}", socket_v4);
+
+  server.await.unwrap();
 }
 
 #[utoipa::path(get, path = "/", responses((status = 200, description = "correctly accessed")))]
