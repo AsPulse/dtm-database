798b33f89fcf3e13904ae40f21390c3d149831f9
あすぱる <84216737+AsPulse@users.noreply.github.com>
2023-11-22 22:44:47 +0900

    [feat] export schema and validate schema (#88)

--- dtm-database-backend/src/bin/main.rs
+++ dtm-database-backend/src/bin/main.rs
@@ -1,17 +1,43 @@
 extern crate dtm_database_backend;
 
 use axum::{routing::get, Router};
-use dtm_database_backend::env::{BootingMode, ENV, PORT};
-use std::net::{Ipv4Addr, SocketAddr};
+use dtm_database_backend::{
+  env::{BootingMode, ENV, PORT},
+  openapi::{is_schema_valid, ApiDoc},
+  routes::version::{hello, version},
+};
+use std::{
+  env,
+  net::{Ipv4Addr, SocketAddr},
+};
 use utoipa::OpenApi;
-use utoipa_swagger_ui::SwaggerUi;
 
-const VERSION: &str = "0.0.1";
+use utoipa_swagger_ui::SwaggerUi;
 
 #[tokio::main]
 async fn main() {
   // set port, ipv4, and socket.
   let socket_v4: SocketAddr = SocketAddr::from((Ipv4Addr::UNSPECIFIED, *PORT));
+
+  let frozen_schema =
+    *ENV == BootingMode::Production || env::args().any(|arg| arg == "--frozen-schema");
+  let only_schema_checking = env::args().any(|arg| arg == "--only-schema-checking");
+  if is_schema_valid(frozen_schema) {
+    println!(
+      "{}",
+      if frozen_schema {
+        "OpenAPI schema validation passed."
+      } else {
+        "There are no updates to the OpenAPI schema."
+      }
+    );
+  } else if frozen_schema {
+    panic!("OpenAPI schema is different from one in the file! Try without --frozen-schema to update schema.");
+  }
+
+  if only_schema_checking {
+    return;
+  }
 
   // build our application with a single route
   let app = match *ENV {
@@ -29,17 +55,3 @@ async fn main() {
 
   server.await.unwrap();
 }
-
-#[utoipa::path(get, path = "/", responses((status = 200, description = "correctly accessed")))]
-async fn hello() -> &'static str {
-  "Hello, World!"
-}
-
-#[utoipa::path(get, path = "/version", responses((status = 200, description = "correctly accessed")))]
-async fn version() -> &'static str {
-  VERSION
-}
-
-#[derive(OpenApi)]
-#[openapi(paths(hello, version))]
-struct ApiDoc;
--- dtm-database-backend/src/lib.rs
+++ dtm-database-backend/src/lib.rs
@@ -1,1 +1,3 @@
 pub mod env;
+pub mod openapi;
+pub mod routes;
